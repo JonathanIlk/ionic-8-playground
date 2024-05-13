cd44ea75dd83f4e216f3b32d02139dae84080acc
Jonathan Ilk <ilk.jonathan@googlemail.com>
2024-05-13 09:29:09 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-8-playground',
+  webDir: 'www'
+};
+
+export default config;
